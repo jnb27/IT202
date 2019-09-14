1f2198f9a6701dad9cf2f48cf7f3f69e9650f13e
jnb27 <jnb27@njit.edu>
2019-09-14 15:03:51 -0400

    DB commit

---
+++ initDb.php
@@ -0,0 +1,8 @@
+<?php
+require('conf.php');
+
+
+echo $host;
+
+echo "   + A positive message, I've tried so hard to get a line break for this and I can't so I'll just put some spaces in front." ;
+?>
