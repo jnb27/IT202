f53fe01f92ec7610a6a293aa44911a2f86c6a342
jnb27 <jnb27@njit.edu>
2019-09-18 19:28:13 -0400

    Db update

--- initDb.php
+++ initDb.php
@@ -1,8 +1,27 @@
 <?php
+
+//Loading the Config
+
 require('conf.php');
+echo "Loaded host " . $host;
 
+try{
+	$conn_string = "mysql:host=$host;dbname=$databasename;charset=utf8mb4";
+	$db = new PDO($conn_string, $username, $password);
+	echo "Connected";
+	$query = "create table if not exists `TestUsers`(
+		`id` int auto_increment not null,
+		`username` carchar(3) not null unique,
+		`pin` int default 0, 
+		PRIMARY KEY (`id)
+		) CHARACTER SET utf8 COLLATE utf8_general_ci";
+	$stmt = $db->prepare($query);
+	$r = $stmt->execute();
+	echo "<br>" . $r . "<br>";
+}
+catch(Exception $e){
+	echo $e->getMessage();
+	echo "Something went wrong";
+}
 
-echo $host;
-
-echo "   + A positive message, I've tried so hard to get a line break for this and I can't so I'll just put some spaces in front." ;
 ?>
