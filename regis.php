f56896d22929af6789714c7849327f2b0224cd4c
jnb27 <jnb27@njit.edu>
2019-10-14 23:27:30 -0400

    regis update

--- regis.php
+++ regis.php
@@ -33,7 +33,7 @@ function myValidation(inputEle, checkValue){
 
  
 <input name="email" type="email" placeholder="Enter your email"/>
-<input name="emailconfirm" type="email" placeholder="Confirm your email" onchange="myValidation(this,email.value/>
+<input name="emailconfirm" type="email" placeholder="Confirm your email" onchange="myValidation(this,email.value)"/>
 <input name="password" type="password" placeholder="Enter your password"/>
 <input name="passwordconfirm" type="password" placeholder="Confirm password"/>
 <input name="username" placeholder="Create username"/>
