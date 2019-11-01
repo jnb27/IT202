e522d9b87e0c0778e820133d3cc1f2ebf358ec1e
jnb27 <jnb27@njit.edu>
2019-10-31 21:29:19 -0400

    LoginTest file

---
+++ LoginTest.php
@@ -0,0 +1,28 @@
+<?php
+
+include('loginscript.php');
+if(isset($_SESSION['login-user'])){
+header("location: profile.php");  }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login Form Milestone</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="login">
+<h2> Login </h2>
+<form action = "" method="post">
+<label>Username: </label>
+<input id="name" name="username" placeholder="uesrname" type="text">
+<label>Password: </label>
+<input id="password" name="password" placeholder="Enter Password" type="password">
+<br></br>
+<input name="submit" type="submit" value="Login">
+<span><?php echo $error; ?></span>
+</form>
+</div>
+</body>
+</html>
