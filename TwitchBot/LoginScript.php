ff5343c76b41101aa25ee796010b414a4da9c894
jnb27 <jnb27@njit.edu>
2019-11-12 10:25:37 -0500

    Folder for Twitch stuff

---
+++ TwitchBot/LoginScript.php
@@ -0,0 +1,33 @@
+<?php
+require('conf.php');
+session_start(); 
+$error = ''; 
+if (isset($_POST['submit'])) {
+if (empty($_POST['username']) || empty($_POST['password'])) {
+$error = "Username or Password is invalid";
+}
+else{
+$username2 = $_POST['username'];
+$password2 = $_POST['password'];
+
+$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+//$db = new PDO($conn_string, $username, $password);
+$conn = mysqli_connect("$host", "$username", "$password", "$database");
+
+
+//Make edits to try and get this working using PDO connections rather than MySQLi
+// Don't use pin anymore because password hashing is a thing
+$query = "SELECT username, pin from TestUsers where username=? AND pin=? LIMIT 1";
+$stmt= $conn->prepare($query);
+$stmt->bind_param("ss", $username2, $password2);
+$stmt->execute();
+//$stmt->bind_result($username2, $password2);
+
+$stmt->store_result();
+if($stmt->fetch())
+$_SESSION['login_user'] = $username2;
+header("location: Profile.php");
+}
+}
+?>
+
