1ee66255bd249ba67c875ed052115748b6ac2b94
jnb27 <jnb27@njit.edu>
2019-11-22 10:47:09 -0500

    WIP Regis page

--- TwitchBot/PDOScript.php
+++ TwitchBot/PDOScript.php
@@ -3,6 +3,7 @@
     ini_set('display_errors',1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
+    $error = ''; 
 ?>
 
 <html>
@@ -27,8 +28,12 @@
 </html>
 
 <?php
-      require(conf.php);
-      if(isset($_POST['username'] && isset($_POST['password'])){
+      require('conf.php');
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+      if (empty($_POST['username']) || empty($_POST['password'])) {
+          $error = "Username or Password is invalid";
+          }
+      if(isset($_POST['username']) && isset($_POST['password'])){
       
         $loginUser = $_POST['username'];
         $loginPass = $_POST['password'];
@@ -44,7 +49,7 @@
               
               if($results && count($results) > 0){
                   //$hashedPassword = password_hash($loginPass, PASSWORD_BCRYPT);
-                  if(password_verify($loginPass, $results['Password']){
+                  if(password_verify($loginPass, $results['Password'])){
                   
                   $_SESSION['login_user'] = $loginUser;
                   header("location: Profile.php");}
@@ -57,6 +62,7 @@
             echo $e->getMessage();
           }
                   
+                  }
                   }
                   
 ?>
--- TwitchBot/Register.php
+++ TwitchBot/Register.php
@@ -1,13 +1,58 @@
 <?php
+      session_start();
+      require(conf.php);
+  
+      if(isset($_POST['register'])){
+          
+          if(!empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['confirm']) ){
+          
+          $newUser = $_POST(['username']);
+          $newPass = $_POST(['password']);
+          $confpass = $_POST(['confirm']);
+          $email = $_POST(['email']);
+          
+          if($newPass === $confpass)
+            {
+               //Check if username exists if not, insert and hash password 
+               
+               $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+               $db = new PDO($conn_string, $username, $password);
+               
+               $sql =  "SELECT COUNT('Username') FROM 'BotUsers' WHERE 'Username' = :username LIMIT 1");
+               $stmt = $db->prepare($sql);
+               
+               $stmt->bindParam(':username', $newUser);
+               $stmt->execute();
+               
+               $results = $stmt->fetch(PDO::FETCH_ASSOC);++
+               
+               if($results > 0)
+               {echo 'User taken'; 
+                 exit();}
+               
+               $hash = password_hash($confpass, PASSWORD_BCRYPT);
+               
+               $aql = "INSERT INTO BotUsers (Username, Password, Email, Coins) VALUES (:username, :password, :email, 100)";
+               $stmt = $db->prepare($sql);
+               
+               $stmt->bindParam(':username', $newUser);
+               $stmt->bindParam(':password', $hash);
+               $stmt->bindParam('email', $email);
+               
+               $result = $stmt->execute();
+               
+               if($result){
+                   echo 'It worked';
+               }
+          
+          
+            }
+          
+              }
+              
+          }
+        
 
-require(conf.php);
+?>
 
-$newuser = $newpass = $confirmpass = "";
-$usererror = $passerror = $confirmerror = "";
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-    if(empty($_POST["$newuser"]))
-      { $usererror = "Please enter a user name.";}
-    else{
-      $sql =
