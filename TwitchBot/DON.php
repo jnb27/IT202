29bb6dbd5fe9fd2c0703b898b2d8344b9029fefc
jnb27 <jnb27@njit.edu>
2019-11-25 23:02:33 -0500

    DON Update

--- TwitchBot/DON.php
+++ TwitchBot/DON.php
@@ -6,15 +6,63 @@
 <body>
 <h1> Welcome to the Double Or Nothing </h1>
 <h2> Instructions </h2>
-<p> This game is very simple, first you bet some coins, and then a coinflip will be done</p>
+<p> This game is very simple, first you bet 5 coins, and then a coinflip will be done</p>
 <p> As the user your only job is to pick one of the two buttons, which will be your guess. Is the next number generated higher or lower? If you get it right you earn coins, you are also able to roll again and double your payout.</p>
+<p id="result">Results will be placed here </p>
 </body>
 
 <button onclick="window.location.href = 'https://web.njit.edu/~jnb27/IT202/TwitchBot/Profile.php';">Back To Profile</button>
 <script>
 
+var coinsBet;
+var coinsEarned;
+var HouseNumber;
+
+function TakeWinnings()
+{
+//Add coins Earned to coins in database
 
 
+}
+
+function BetHeads()
+{
+    HouseNumber = Math.floor(Math.random() * 101);
+    if(HouseNumber < 50)
+    {
+      //This result is heads
+      document.getElementById("result").innerHTML = 'You correctly guessed heads, do you want to try and double it? If not, please hit take winnings.';
+      coinsEarned = coinsBet * 2;
+    }
+    else{
+      document.getElementById("result").innerHTML = 'Your guess was incorrect. It was actually tails';
+      //Subtract 5 coins
+      coinsEarned = 0;
+      }
+    
+    
+}
+
+
+
+function BetTails()
+{
+    HouseNumber = Math.floor(Math.random() * 101);
+    if(HouseNumber >= 50)
+    {
+      //This result is tails
+       document.getElementById("result").innerHTML = 'You correctly guessed tails, do you want to try and double it?';
+    }
+    else{
+     document.getElementById("result").innerHTML = 'Your guess was incorrect. It was actually heads.';
+     //Subtract Coins
+     coinsEarned = 0;
+    }
+}
 
 </script>
+
+<button type="button" onclick="BetHeads()">Heads</button>
+<button type="button" onclick="BetTails()">Tails</button>
+
 </html>
