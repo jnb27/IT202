634dd1565c7f993d0ea370f0e122704f780aac96
jnb27 <jnb27@njit.edu>
2019-11-25 19:19:52 -0500

    Games

---
+++ TwitchBot/DON.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Double Or Nothing</title>
+</head>
+<body>
+<h1> Welcome to the Double Or Nothing </h1>
+<h2> Instructions </h2>
+<p> This game is very simple, first you bet some coins, and then a coinflip will be done</p>
+<p> As the user your only job is to pick one of the two buttons, which will be your guess. Is the next number generated higher or lower? If you get it right you earn coins, you are also able to roll again and double your payout.</p>
+</body>
+
+<button onclick="window.location.href = 'https://web.njit.edu/~jnb27/IT202/TwitchBot/Profile.php';">Back To Profile</button>
+<script>
+
+
+
+
+</script>
+</html>
--- TwitchBot/HL.php
+++ TwitchBot/HL.php
@@ -8,12 +8,12 @@
 <h2> Instructions </h2>
 <p> This game is very simple, once you hit start a number will be randomly generated.</p>
 <p> As the user your only job is to pick one of the two buttons, which will be your guess. Is the next number generated higher or lower? If you get it right you earn coins. </p>
+</body>
 
-<form action="https://web.njit.edu/~jnb27/IT202/TwitchBot/Profile.php">
-    <input type="submit" value="Back to Profile"/>
-</form>
+<button onclick="window.location.href = 'https://web.njit.edu/~jnb27/IT202/TwitchBot/Profile.php';">Back To Profile</button>
 
-<button id="Start">Start Game</b>
+<p id ="NumHolder"> The first number will be generated here</p>
+<p id ="NextNumber"> Your number will be place here </p>
 
 <script>
 
@@ -23,17 +23,64 @@ var UserGuess;
 
 function StartGame()
 {
-
+NumStart = Math.floor(Math.random() * 101);
+document.getElementById("NumHolder").innerHTML = NumStart;
+document.getElementById("NextNumber").innerHTML = 'Your number';
 }
 
-function Higher()
+function HigherGuess()
 {
+  if(NumStart == null)
+    {alert("You should start the game first.."); }
+    else{
+    NextNum = Math.floor(Math.random() * 101);
+    document.getElementById("NextNumber").innerHTML = NextNum;
+    
     if(NextNum > NumStart)
       {
-        window.alert()
+      //When win add coins
+      
+        NumStart = null;
+        alert("You won!");
+        document.getElementById("NumHolder").innerHTML = NumStart;
       }
+      else{
+        NumStart = null;
+        alert("Sorry you lost.");
+        document.getElementById("NumHolder").innerHTML = NumStart;
+      }
+    }
 }
 
+function LowerGuess()
+{
+    if(NumStart == null)
+      { alert("You should probably start the game first.."); }
+      else{
+      NextNum = Math.floor(Math.random() * 101);
+    
+    document.getElementById("NextNumber").innerHTML = NextNum;
+    if(NextNum < NumStart)
+      {
+        //When win add coins
+        
+        NumStart = null;
+        alert("You won!");
+        document.getElementById("NumHolder").innerHTML = NumStart;
+        
+      }
+      else{
+        NumStart = null;
+        alert("Sorry you lost.");
+        document.getElementById("NumHolder").innerHTML = NumStart;
+        
+      }
+    }
+}
 </script>
-</body>
+
+<button type="button" onclick="StartGame()">Start</button>
+<button type="button" onclick="HigherGuess()">Higher</button>
+<button type="button" onclick="LowerGuess()">Lower</button>
+
 </html>
