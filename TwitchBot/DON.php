469aaeb3bb54b4292557b0cd35692db332d75af9
jnb27 <jnb27@njit.edu>
2019-11-26 21:56:24 -0500

    Stuff works now

--- TwitchBot/DON.php
+++ TwitchBot/DON.php
@@ -9,7 +9,7 @@
 <p> This game is very simple, first you bet 5 coins, and then a coinflip will be done</p>
 <p> As the user your only job is to pick one of the two buttons, which will be your guess. Is the next number generated higher or lower? If you get it right you earn coins, you are also able to roll again and double your payout.</p>
 <p id="result">Results will be placed here </p>
-<p id="AjaxTest">Connection testing here </p>
+<p id="AjaxTest"></p>
 </body>
 
 <button onclick="window.location.href = 'https://web.njit.edu/~jnb27/IT202/TwitchBot/Profile.php';">Back To Profile</button>
@@ -36,6 +36,21 @@ xhttp.send();
 document.getElementById("Claim").style.visibility = "hidden";
 }
 
+function LostCoins()
+{
+let xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function(){
+      if(this.readyState == 4 && this.status == 200)
+          {
+          document.getElementById("AjaxTest").innerHTML = this.responseText;
+          }
+      }
+
+xhttp.open("POST", "UpdateCoins2.php",true);
+xhttp.send();
+}
+
 function BetHeads()
 {
     HouseNumber = Math.floor(Math.random() * 101);
@@ -49,6 +64,7 @@ function BetHeads()
       document.getElementById("result").innerHTML = 'Your guess was incorrect. It was actually tails';
       //Subtract 5 coins
       coinsEarned = 0;
+      LostCoins();
       }
     
     
@@ -68,6 +84,7 @@ function BetTails()
      document.getElementById("result").innerHTML = 'Your guess was incorrect. It was actually heads.';
      //Subtract Coins
      coinsEarned = 0;
+     LostCoins();
     }
 }
 
---
+++ TwitchBot/UpdateCoins2.php
@@ -0,0 +1,36 @@
+
+<?php
+require('conf.php');
+session_start();
+
+$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+$db = new PDO($conn_string, $username, $password);
+
+
+if(!isset($_SESSION['login_user'])){
+header("location: LoginTest.php");
+}
+
+$user_check = $_SESSION['login_user'];
+
+$into = "SELECT * FROM BotUsers WHERE Username = :user_check";
+$result = $db->prepare($into);
+$result->execute(array(":user_check"=>$user_check));
+
+$row = $result->fetch(PDO::FETCH_ASSOC);
+
+$login_session = $row['Username'];
+$CurrentCoins = $row['Coins'];
+
+//If win a game give coins
+
+$CoinsLost = 10;
+
+$CoinTotal = $CurrentCoins - $CoinsLost;
+
+$update = "UPDATE `jnb27`.`BotUsers` SET `Coins` = '$CoinTotal' WHERE `BotUsers`.`Username` = '$user_check'";
+$result2 = $db->prepare($update);
+$result2->execute();
+
+
+?>
